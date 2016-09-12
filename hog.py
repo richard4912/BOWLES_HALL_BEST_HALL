9291ba33bc405a81a11b7fe1b84e281b6995138c
Daniel Ho <danielho6868@gmail.com>
2016-09-12 11:45:24 -0700

    added hog.py

---
+++ hog.py
@@ -0,0 +1,450 @@
+"""CS 61A Presents The Game of Hog."""
+
+from dice import four_sided, six_sided, make_test_dice
+from ucb import main, trace, log_current_line, interact
+
+GOAL_SCORE = 100  # The goal of Hog is to score 100 points.
+
+
+######################
+# Phase 1: Simulator #
+######################
+
+def roll_dice(num_rolls, dice=six_sided):
+    """Simulate rolling the DICE exactly NUM_ROLLS>0 times. Return the sum of
+    the outcomes unless any of the outcomes is 1. In that case, return the
+    number of 1's rolled.
+    """
+    # These assert statements ensure that num_rolls is a positive integer.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls > 0, 'Must roll at least once.'
+    # BEGIN PROBLEM 1
+    total = 0
+    pig_out = False
+    while num_rolls > 0:
+        roll = dice()
+        if pig_out:
+            if roll == 1:
+                total += 1
+        elif roll == 1:
+            pig_out = True
+            total = 1
+        else:
+            total += roll
+        num_rolls -= 1
+    return total
+    # END PROBLEM 1
+
+def free_bacon(opponent_score):
+    """Return the points scored from rolling 0 dice (Free Bacon)."""
+    # BEGIN PROBLEM 2
+    largest = 0
+    while opponent_score > 0:
+        if opponent_score % 10 > largest:
+            largest = opponent_score % 10
+        opponent_score //= 10
+    return largest + 1
+    # END PROBLEM 2
+
+# Write your prime functions here!
+def is_prime(num):
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+    if not num % 2:
+        return False
+
+    for x in range(3, int(num**.5)+1, 2):
+        if not num % x:
+            return False
+    return True
+
+def next_prime(num):
+    if num == 2:
+        return 3
+
+    prime = num + 2
+    while True:
+        if is_prime(prime):
+            return prime
+        prime += 2
+
+def take_turn(num_rolls, opponent_score, dice=six_sided):
+    """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free Bacon).
+    Return the points scored for the turn by the current player. Also
+    implements the Hogtimus Prime and When Pigs Fly rules.
+
+    num_rolls:       The number of dice rolls that will be made.
+    opponent_score:  The total score of the opponent.
+    dice:            A function of no args that returns an integer outcome.
+    """
+    # Leave these assert statements here; they help check for errors.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
+    assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
+    assert opponent_score < 100, 'The game should be over.'
+    # BEGIN PROBLEM 2
+    if num_rolls == 0:
+        score = free_bacon(opponent_score)
+    else:
+        score = roll_dice(num_rolls, dice)
+    if is_prime(score):
+        score = next_prime(score)
+    
+    if score > 25 - num_rolls:
+        return 25 - num_rolls
+    else:
+        return score
+    # END PROBLEM 2
+
+
+def reroll(dice):
+    """Return dice that return even outcomes and re-roll odd outcomes of DICE."""
+    def rerolled():
+        # BEGIN PROBLEM 3
+        roll = dice()
+        if roll % 2 != 0:
+            return dice()
+        else:
+            return roll
+        # END PROBLEM 3
+    return rerolled
+
+
+def select_dice(score, opponent_score, dice_swapped):
+    """Return the dice used for a turn, which may be re-rolled (Hog Wild) and/or
+    swapped for four-sided dice (Pork Chop).
+
+    DICE_SWAPPED is True if and only if four-sided dice are being used.
+    """
+    # BEGIN PROBLEM 4
+    if dice_swapped:
+        dice = four_sided
+    else:
+        dice = six_sided  # Replace this statement
+    # END PROBLEM 4
+    if (score + opponent_score) % 7 == 0:
+        dice = reroll(dice)
+    return dice
+
+
+def other(player):
+    """Return the other player, for a player PLAYER numbered 0 or 1.
+
+    >>> other(0)
+    1
+    >>> other(1)
+    0
+    """
+    return 1 - player
+
+
+def play(strategy0, strategy1, score0=0, score1=0, goal=GOAL_SCORE):
+    """Simulate a game and return the final scores of both players, with
+    Player 0's score first, and Player 1's score second.
+
+    A strategy is a function that takes two total scores as arguments
+    (the current player's score, and the opponent's score), and returns a
+    number of dice that the current player will roll this turn.
+
+    strategy0:  The strategy function for Player 0, who plays first
+    strategy1:  The strategy function for Player 1, who plays second
+    score0   :  The starting score for Player 0
+    score1   :  The starting score for Player 1
+    """
+    player = 0  # Which player is about to take a turn, 0 (first) or 1 (second)
+    dice_swapped = False  # Whether 4-sided dice have been swapped for 6-sided
+    # BEGIN PROBLEM 5
+    while score0 < goal and score1 < goal:
+        if player == 0:
+            num_roll = strategy0(score0, score1)
+            if num_roll == -1:
+                dice_swapped = not dice_swapped
+                score0 += 1
+            else:
+                score0 += take_turn(num_roll, score1, select_dice(score0, score1, dice_swapped))
+        else:
+            #player 1
+            num_roll = strategy1(score1, score0)
+            if num_roll == -1:
+                dice_swapped = not dice_swapped
+                score1 +=1
+            else:
+                score1 += take_turn(num_roll, score0, select_dice(score1, score0, dice_swapped))
+
+        #swine swap
+        if score0 == 2*score1 or score1 == 2*score0:
+            score0, score1 = score1, score0
+
+        player = other(player)
+    # END PROBLEM 5
+    return score0, score1
+
+
+#######################
+# Phase 2: Strategies #
+#######################
+
+def always_roll(n):
+    """Return a strategy that always rolls N dice.
+
+    A strategy is a function that takes two total scores as arguments
+    (the current player's score, and the opponent's score), and returns a
+    number of dice that the current player will roll this turn.
+
+    >>> strategy = always_roll(5)
+    >>> strategy(0, 0)
+    5
+    >>> strategy(99, 99)
+    5
+    """
+    def strategy(score, opponent_score):
+        return n
+    return strategy
+
+
+def check_strategy_roll(score, opponent_score, num_rolls):
+    """Raises an error with a helpful message if NUM_ROLLS is an invalid
+    strategy output. All strategy outputs must be integers from -1 to 10.
+
+    >>> check_strategy_roll(10, 20, num_rolls=100)
+    Traceback (most recent call last):
+     ...
+    AssertionError: strategy(10, 20) returned 100 (invalid number of rolls)
+
+    >>> check_strategy_roll(20, 10, num_rolls=0.1)
+    Traceback (most recent call last):
+     ...
+    AssertionError: strategy(20, 10) returned 0.1 (not an integer)
+
+    >>> check_strategy_roll(0, 0, num_rolls=None)
+    Traceback (most recent call last):
+     ...
+    AssertionError: strategy(0, 0) returned None (not an integer)
+    """
+    msg = 'strategy({}, {}) returned {}'.format(
+        score, opponent_score, num_rolls)
+    assert type(num_rolls) == int, msg + ' (not an integer)'
+    assert -1 <= num_rolls <= 10, msg + ' (invalid number of rolls)'
+
+
+def check_strategy(strategy, goal=GOAL_SCORE):
+    """Checks the strategy with all valid inputs and verifies that the
+    strategy returns a valid input. Use `check_strategy_roll` to raise
+    an error with a helpful message if the strategy returns an invalid
+    output.
+
+    >>> def fail_15_20(score, opponent_score):
+    ...     if score != 15 or opponent_score != 20:
+    ...         return 5
+    ...
+    >>> check_strategy(fail_15_20)
+    Traceback (most recent call last):
+     ...
+    AssertionError: strategy(15, 20) returned None (not an integer)
+    >>> def fail_102_115(score, opponent_score):
+    ...     if score == 102 and opponent_score == 115:
+    ...         return 100
+    ...     return 5
+    ...
+    >>> check_strategy(fail_102_115)
+    >>> fail_102_115 == check_strategy(fail_102_115, 120)
+    Traceback (most recent call last):
+     ...
+    AssertionError: strategy(102, 115) returned 100 (invalid number of rolls)
+    """
+    # BEGIN PROBLEM 6
+    for x in range (-1,goal):
+        for y in range (-1,goal):
+            check_strategy_roll(x,y,strategy(x,y))
+    # END PROBLEM 6
+
+
+# Experiments
+
+def make_averaged(fn, num_samples=1000):
+    """Return a function that returns the average_value of FN when called.
+
+    To implement this function, you will have to use *args syntax, a new Python
+    feature introduced in this project.  See the project description.
+
+    >>> dice = make_test_dice(3, 1, 5, 6)
+    >>> averaged_dice = make_averaged(dice, 1000)
+    >>> averaged_dice()
+    3.75
+    """
+    # BEGIN PROBLEM 7
+    def average_func(*args):
+        trials = num_samples
+        total = 0
+        while trials > 0:
+            total += fn(*args)
+            trials -= 1
+        return total / num_samples
+    return average_func
+    # END PROBLEM 7
+
+
+def max_scoring_num_rolls(dice=six_sided, num_samples=10000):
+    """Return the number of dice (1 to 10) that gives the highest average turn
+    score by calling roll_dice with the provided DICE over NUM_SAMPLES times.
+    Assume that the dice always return positive outcomes.
+
+    >>> dice = make_test_dice(3)
+    >>> max_scoring_num_rolls(dice)
+    10
+    """
+    # BEGIN PROBLEM 8
+    roll_doer = make_averaged(roll_dice, num_samples)
+    num_dice, best_roll = 0, 0
+    for x in range(1,11):
+        trial = roll_doer(x, dice)
+        if trial > best_roll:
+            num_dice, best_roll = x, trial
+    return num_dice
+    # END PROBLEM 8
+
+
+def winner(strategy0, strategy1):
+    """Return 0 if strategy0 wins against strategy1, and 1 otherwise."""
+    score0, score1 = play(strategy0, strategy1)
+    if score0 > score1:
+        return 0
+    else:
+        return 1
+
+
+def average_win_rate(strategy, baseline=always_roll(4)):
+    """Return the average win rate of STRATEGY against BASELINE. Averages the
+    winrate when starting the game as player 0 and as player 1.
+    """
+    win_rate_as_player_0 = 1 - make_averaged(winner)(strategy, baseline)
+    win_rate_as_player_1 = make_averaged(winner)(baseline, strategy)
+
+    return (win_rate_as_player_0 + win_rate_as_player_1) / 2
+
+
+def run_experiments():
+    """Run a series of strategy experiments and report results."""
+    if False:  # Change to False when done finding max_scoring_num_rolls
+        six_sided_max = max_scoring_num_rolls(six_sided)
+        print('Max scoring num rolls for six-sided dice:', six_sided_max)
+        rerolled_max = max_scoring_num_rolls(reroll(six_sided))
+        print('Max scoring num rolls for re-rolled dice:', rerolled_max)
+
+    if False:  # Change to False when done finding max_scoring_num_rolls
+        four_sided_max = max_scoring_num_rolls(four_sided)
+        print('Max scoring num rolls for four-sided dice:', four_sided_max)
+        #rerolled_max = max_scoring_num_rolls(reroll(four_sided))
+        #print('Max scoring num rolls for re-rolled dice:', rerolled_max)
+
+    if False:  # Change to True to test always_roll(8)
+        print('always_roll(8) win rate:', average_win_rate(always_roll(8)))
+
+    if False:  # Change to True to test bacon_strategy
+        print('bacon_strategy win rate:', average_win_rate(bacon_strategy))
+
+    if False:  # Change to True to test swap_strategy
+        print('swap_strategy win rate:', average_win_rate(swap_strategy))
+
+    if True:
+        print('my strategy WR:', average_win_rate(final_strategy))
+
+
+# Strategies
+
+def bacon_strategy(score, opponent_score, margin=8, num_rolls=4):
+    """This strategy rolls 0 dice if that gives at least MARGIN points,
+    and rolls NUM_ROLLS otherwise.
+    """
+    # BEGIN PROBLEM 9
+    points = free_bacon(opponent_score)
+    if is_prime(points):
+        points = next_prime(points)
+    if points >= margin:
+        return 0
+    else:
+        return num_rolls 
+    # END PROBLEM 9
+check_strategy(bacon_strategy)
+
+
+def swap_strategy(score, opponent_score, margin=8, num_rolls=4):
+    """This strategy rolls 0 dice when it triggers a beneficial swap. It also
+    rolls 0 dice if it gives at least MARGIN points. Otherwise, it rolls
+    NUM_ROLLS.
+    """
+    # BEGIN PROBLEM 10
+    points = free_bacon(opponent_score)
+    if is_prime(points):
+        points = next_prime(points)
+
+    if points >= margin or score + points == 2*opponent_score:
+        return 0
+    else:
+        return num_rolls 
+    # END PROBLEM 10
+check_strategy(swap_strategy)
+
+
+def final_strategy(score, opponent_score):
+    """Switch the dice to the four sided dice on the first turn from the pork chop rule.
+    Then, calculate what would happen to your score if you were to roll 0 dice.
+
+    Now, based on how many points you score, decide if you want to 
+    1) roll 0 score because you get enough points from free bacon, and from a potential swine swap / hogtimus prime
+    2) roll 5 dice as a baseline
+    3) roll 4 dice to reduce variance when you are about to win
+    4) roll 8 dice to take advantage of a potential hog wild re-roll dice
+
+    """
+    # BEGIN PROBLEM 11
+    if score == 0:
+        return -1
+
+    points = free_bacon(opponent_score)
+    if is_prime(points):
+        points = next_prime(points)
+    zero_score = score + points
+
+    if zero_score == 2*opponent_score and points >= 15:
+        return 0
+    elif zero_score == 2*opponent_score and points >= 10 and zero_score + opponent_score % 7 != 0:
+        return 0
+    elif points >= 9:
+        return 0
+    elif score + opponent_score % 7 == 0:
+        return 8
+    elif points >= 5 and zero_score + opponent_score % 7 != 0:
+        return 0
+    else:
+        if score > 90:
+            return 4
+        return 5
+    # END PROBLEM 11
+check_strategy(final_strategy)
+
+
+##########################
+# Command Line Interface #
+##########################
+
+# NOTE: Functions in this section do not need to be changed. They use features
+# of Python not yet covered in the course.
+
+@main
+def run(*args):
+    """Read in the command-line argument and calls corresponding functions.
+
+    This function uses Python syntax/techniques not yet covered in this course.
+    """
+    import argparse
+    parser = argparse.ArgumentParser(description="Play Hog")
+    parser.add_argument('--run_experiments', '-r', action='store_true',
+                        help='Runs strategy experiments')
+
+    args = parser.parse_args()
+
+    if args.run_experiments:
+        run_experiments()
